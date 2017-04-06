0f7b3388429f08b0e73639d93c9bc7a9b1c387bf
LAPTOP-D3N7RC75\lenovo <867763618@qq.com>
2017-04-06 10:00:45 +0800

    配置文件

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,9 @@
 var path = require('path');
 
 module.exports = {
-	entry: 'app/index.js',
+	entry: './app/index.js',
 	output: {
 		filename: 'bundle.js',
-		path: path.resolv(__dirname, 'dist')
+		path: path.resolve(__dirname, 'dist')
 	}
 };
